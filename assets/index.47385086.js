5328d896ee551d575b54a5e67b7d6de29c4279a8
Костя <kostia.freelancer@gmail.com>
2022-08-22 10:43:14 +0300

    Updates

---
+++ assets/index.47385086.js
@@ -0,0 +1,1 @@
+import{C as a}from"./Card.f9542172.js";import"./index.95844218.js";export{a as default};
--- assets/index.c226961f.js
+++
@@ -1,1 +0,0 @@
-import{C as a}from"./Card.1bfdabf0.js";import"./index.03678239.js";export{a as default};
