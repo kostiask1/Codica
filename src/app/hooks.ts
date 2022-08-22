a4eaa3c6c35550c966fef08176efff5f154788d0
Костя <kostia.freelancer@gmail.com>
2022-08-22 17:11:52 +0300

    useDebounce width

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,23 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useEffect, useState } from "react"
 import type { TypedUseSelectorHook } from "react-redux"
-import type { RootState, AppDispatch } from "./store"
+import { useDispatch, useSelector } from "react-redux"
+import type { AppDispatch, RootState } from "./store"
 
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export const useDebounce = (value: any, delay = 300) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay])
+
+  return debouncedValue
+}
--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,12 +1,21 @@
 import { Grid } from "@mui/material"
-import { useAppSelector } from "../../app/hooks"
+import { useAppSelector, useDebounce } from "../../app/hooks"
 import { RootState } from "../../app/store"
 import Card from "../../components/Card"
 import CityForm from "../../components/CityForm"
 import { ICity } from "../../types"
+import { useEffect, useState } from "react"
 
 const List = () => {
   const cities = useAppSelector((state: RootState) => state.cities.array)
+  const [width, setWidth] = useState(0)
+  const debouncedWidth = useDebounce(width, 500)
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
   return (
     <>
       <CityForm />
@@ -15,7 +24,7 @@ const List = () => {
           cities.map(
             (city: ICity) =>
               city.show && (
-                <Grid item xs={6} key={city.id}>
+                <Grid item xs={debouncedWidth > 767 ? 6 : 12} key={city.id}>
                   <Card city={city.name} />
                 </Grid>
               )
